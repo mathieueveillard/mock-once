25b70720f1d56aac95b1a515eee10597e09f45ad
Mathieu Eveillard <mathieu.eveillard@gmail.com>
2021-10-05 22:42:05 +0200

    Gist

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,7 +1,23 @@
-import { systemUnderTest } from ".";
+import { mock, mockOnce } from ".";
 
-describe("Test of systemUnderTest()", function() {
-  it("Should pass", function() {
-    expect(systemUnderTest()).toEqual(true);
-  });
+it("should use the function provided as argument as an implementation", function () {
+  const fn = () => 1;
+  const mockedFn = mock(fn);
+  expect(mockedFn()).toEqual(1);
+});
+
+it("should use the 2 functions provided as argument as implementations", function () {
+  const fn1 = () => 1;
+  const fn2 = () => 2;
+  const mockedFn = mock(fn1, fn2);
+  expect(mockedFn()).toEqual(1);
+  expect(mockedFn()).toEqual(2);
+});
+
+it("should use the 2 functions provided as argument as implementations, with fluent API", function () {
+  const fn1 = () => 1;
+  const fn2 = () => 2;
+  const mockedFn = mockOnce(fn1).mockOnce(fn2);
+  expect(mockedFn()).toEqual(1);
+  expect(mockedFn()).toEqual(2);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,19 @@
-export function systemUnderTest(): boolean {
-  return true;
+export function mock<T>(...fns: (() => T)[]): () => T {
+  let numberOfInvokations = 0;
+  return function () {
+    return fns[numberOfInvokations++]();
+  };
+}
+
+export interface FluentApi<T> {
+  (): T;
+  mockOnce(fn: () => T): FluentApi<T>;
+}
+
+export function mockOnce<T>(...fns: (() => T)[]): FluentApi<T> {
+  const mockedFn = mock(...fns);
+  function next(fn: () => T): FluentApi<T> {
+    return mockOnce(...fns, fn);
+  }
+  return Object.assign(mockedFn, { mockOnce: next });
 }
